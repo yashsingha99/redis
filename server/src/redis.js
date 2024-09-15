d2e92f0e651b4fb2d4a1d8b2dddf5c644f62646c
Yash Singhal <singhaly914@gmail.com>
2024-09-16 01:00:08 +0530

    add

--- server/src/redis.js
+++ server/src/redis.js
@@ -1,5 +1,10 @@
 const { Redis } = require("ioredis");
 
-const client = new Redis()
-
+const client = new Redis({
+    port: process.env.REDIS_PORT,
+    host: process.env.REDIS_HOST,
+    username: process.env.REDIS_USER,
+    password: process.env.REDIS_PASSWORD
+})
+client.connect(() => console.log("redis connected"))
 module.exports = client
